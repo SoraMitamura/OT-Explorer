3c2880ecc94d3dafee4cd45211f39e3952054afd
SoraMitamura <74039446+SoraMitamura@users.noreply.github.com>
2026-05-08 21:46:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -207,10 +207,10 @@ REMOVE TOP PADDING
 # SIZE PARAMETERS
 # =========================================================
 
-coronal_width = 900
+coronal_width = 700
 coronal_height = 440
 
-dorsal_width = 900
+dorsal_width = 700
 dorsal_height = 440
 
 gap = 0.24

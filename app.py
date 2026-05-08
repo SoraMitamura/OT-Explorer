78a52b739cb8b803ee56a53b767364db57705193
SoraMitamura <74039446+SoraMitamura@users.noreply.github.com>
2026-05-08 22:28:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -526,7 +526,7 @@ fig.update_layout(
 # MAIN LAYOUT
 # =========================================================
 
-plot_col, stat_col = st.columns([4,2])
+plot_col, stat_col = st.columns([4,2.5])
 
 # =========================================================
 # PLOT

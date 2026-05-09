c97775acef25053873d8cde3ab127a7f2959f502
SoraMitamura <74039446+SoraMitamura@users.noreply.github.com>
2026-05-10 01:25:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -886,28 +886,23 @@ with stat_col:
     )
 
     st.markdown("""
-    <a href="https://ot-explorer-tgy2kpdwd2jhmfnmfyxdvc.streamlit.app/"
-    target="_self"
-    style="
-    display:block;
-    text-align:center;
-    padding:10px 16px;
-    border-radius:14px;
-    background:#111827;
-    color:white;
-    text-decoration:none;
-    border:1px solid rgba(255,255,255,0.08);
-    width:50%;
-    margin:auto;
-    ">
-    MERFISH only
-    </a>
-    """, unsafe_allow_html=True)
-
-    st.markdown(
-        "<div style='height:20px'></div>",
-        unsafe_allow_html=True
-    )
+<a href="https://ot-explorer-tgy2kpdwd2jhmfnmfyxdvc.streamlit.app/"
+target="_self"
+style="
+display:block;
+text-align:center;
+padding:10px 16px;
+border-radius:14px;
+background:#111827;
+color:white;
+text-decoration:none;
+border:1px solid rgba(255,255,255,0.08);
+width:100%;
+box-sizing:border-box;
+">
+MERFISH only
+</a>
+""", unsafe_allow_html=True)
 
     st.link_button(
         "What is OT?",

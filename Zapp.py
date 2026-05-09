a734d17e278208bc035f5b4caa05fd07c134fbf2
SoraMitamura <74039446+SoraMitamura@users.noreply.github.com>
2026-05-10 00:46:47 +0900

    Update Zapp.py

--- Zapp.py
+++ Zapp.py
@@ -48,7 +48,21 @@ div.stLinkButton a:hover {
 
     color: white !important;
 }
+/* =======================================================
+BUTTON SPACING
+======================================================= */
 
+div.stButton {
+    margin-bottom: -12px;
+}
+
+div.stDownloadButton {
+    margin-bottom: -12px;
+}
+
+div.stLinkButton {
+    margin-bottom: -12px;
+}
 </style>
 """, unsafe_allow_html=True)
 st.markdown("""

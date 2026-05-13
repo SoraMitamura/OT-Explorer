d3bbd4744c464be84f1daaa180f951f4f14bd1a0
SoraMitamura <SoraMitamura@users.noreply.github.com>
2026-05-13 20:56:36 +0900

    many

--- Papp.py
+++ Papp.py
@@ -24,9 +24,9 @@ st.caption("Olfactory Tubercle literature explorer")
 @st.cache_data
 def load_data():
     return pd.read_csv(
-    "papers.csv",
-    encoding="utf-8-sig"
-)
+        "papers.csv",
+        encoding="utf-8-sig"
+    )
 
 df = load_data()
 
